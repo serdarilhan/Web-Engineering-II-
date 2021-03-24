d18f1a48fb239d5905031dff70e8a9b64f3dd368
serdarilhan <inf19223@lehre.dhbw-stuttgart.de>
2021-03-24 17:51:57 +0100

    Update2

--- controllers/walletBackend.js
+++ controllers/walletBackend.js
@@ -25,49 +25,61 @@ exports.wallet = (req, res) => {
         console.log(result);
         var loggedInCrypto = result[0].crypto;
         if (loggedInCrypto == adresse) {
-            res.render("wallet",{
-                message0 : "Fehler, es ist ihre Adresse!!!"
+            res.render("wallet", {
+                message0: "Fehler, es ist ihre Adresse!!!"
             });
             console.log("Fehler es ist ihre Adresse!!!");
-        } else {
-            db.query("SELECT kontostand FROM user WHERE email = ?", [email] , function (err, result) {
-                if (err) {
-                    console.log(err);
-                }
-                var max_send_money = result[0].kontostand;
-                console.log(max_send_money);
+        }
+        db.query("SELECT crypto FROM user WHERE crypto = ?", [adresse], function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else if (result[0] !== adresse) {
+                console.log("Keine gültige Adresse");
+                res.render("wallet", { message7: "Keine gültige Adresse" });
+            }
+            else {
+                db.query("SELECT kontostand FROM user WHERE email = ?", [email], function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    var max_send_money = result[0].kontostand;
+                    console.log(max_send_money);
 
-                if (amount > max_send_money) {
-                    res.render("wallet",{
-                        message1: "Sie haben zu wenige Coins!!!"
-                    })
-                    console.log("Sie haben zu wenige Coins!!!");
-                } else {
-                    db.query("SELECT kontostand FROM user WHERE crypto = ?", [adresse], function (err, result) {
-                        if (err) {
-                            console.log(err);
-                        }
-                        var kontostand = result[0].kontostand;
-                        var neuerKontostand = kontostand + amount;
-                        var neuerKontostandSender = max_send_money - amount;
+                    if (amount > max_send_money) {
+                        res.render("wallet", {
+                            message1: "Sie haben zu wenige Coins!!!"
+                        })
+                        console.log("Sie haben zu wenige Coins!!!");
+                    } else {
+                        db.query("SELECT kontostand FROM user WHERE crypto = ?", [adresse], function (err, result) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            var kontostand = result[0].kontostand;
+                            var neuerKontostand = kontostand + amount;
+                            var neuerKontostandSender = max_send_money - amount;
 
-                        var datum = new Date();
-                        
-                        db.query('UPDATE user SET ? WHERE crypto = ?', [{ kontostand: neuerKontostand }, adresse]);
-                        db.query('UPDATE user SET ? WHERE email = ?', [{ kontostand: neuerKontostandSender }, email]);
-                        db.query('INSERT INTO transaction SET ?', { sender: loggedInCrypto, receiver: adresse, amount: amount, date: datum , info : "Transaction" });
-                        res.render("wallet",{
-                            message2 : "Transaktion erfolgreich!!!"
+                            var datum = new Date();
+
+                            db.query('UPDATE user SET ? WHERE crypto = ?', [{ kontostand: neuerKontostand }, adresse]);
+                            db.query('UPDATE user SET ? WHERE email = ?', [{ kontostand: neuerKontostandSender }, email]);
+                            db.query('INSERT INTO transaction SET ?', { sender: loggedInCrypto, receiver: adresse, amount: amount, date: datum, info: "Transaction" });
+                            res.render("wallet", {
+                                message2: "Transaktion erfolgreich!!!"
+                            })
+
                         })
 
-                    })
-                    
-                } 
-            })
-        }
+                    }
+                })
+            }
+        })
     });
 
 }
+
+
 exports.showCrypto = (req, res) => {
     var email = show.email;
     db.query("SELECT crypto FROM user WHERE email = ? ", [email], function (err, result) {
@@ -76,7 +88,7 @@ exports.showCrypto = (req, res) => {
         }
         console.log(result);
         return res.render("wallet", {
-            crypto: "Deine Crypto-Adresse lautet: " + result[0].crypto 
+            crypto: "Deine Crypto-Adresse lautet: " + result[0].crypto
         });
     })
 
@@ -102,10 +114,10 @@ exports.mining = (req, res) => {
             console.log(neuMining);
             console.log(max_send_money);
             var neuerKontostandUser = max_send_money - amountMining;
-          
+
 
             if (amountMining > max_send_money) {
-                res.render("wallet",{
+                res.render("wallet", {
                     message3: "Sie haben zu wenige Coins!!!"
                 })
                 console.log("Sie haben zu wenige Coins!!!");
@@ -113,88 +125,88 @@ exports.mining = (req, res) => {
                 db.query('UPDATE user SET ? WHERE email = ?', [{ mining: neuMining }, email]);
                 db.query('UPDATE user SET ? WHERE email = ?', [{ kontostand: neuerKontostandUser }, email]);
 
-                res.render("wallet",{
-                    message4 : "Mining erfolgreich durchgeführt!!!"
+                res.render("wallet", {
+                    message4: "Mining erfolgreich durchgeführt!!!"
                 })
 
                 var count = neuMining;
-                let max = 1.2575* neuMining;
+                let max = 1.2575 * neuMining;
                 function counter() {
                     if (count <= max) {
-                    count = count + 0.1;
-                    //res.render("wallet",{
-                      //  minen: "Ihre Mining-Funktion: " //Ausbauen ohne error in wallet anzeigen, minen2 : count
-                    //});
-                    console.log(count);
-                    db.query("UPDATE user SET ? WHERE email = ?", [{mining : count}, email]);
+                        count = count + 0.1;
+                        //res.render("wallet",{
+                        //  minen: "Ihre Mining-Funktion: " //Ausbauen ohne error in wallet anzeigen, minen2 : count
+                        //});
+                        console.log(count);
+                        db.query("UPDATE user SET ? WHERE email = ?", [{ mining: count }, email]);
                     }
                 }
                 setInterval(counter, 10000);
 
-                db.query("SELECT crypto FROM user WHERE email = ?", [email] , function (err,result) {
-                    if(err){
+                db.query("SELECT crypto FROM user WHERE email = ?", [email], function (err, result) {
+                    if (err) {
                         console.log(err);
                     }
                     var mining_crypto = result[0].crypto;
                     var datum = new Date();
-                     db.query('INSERT INTO transaction SET ?', { sender: mining_crypto, receiver: mining_crypto, amount: amountMining, date: datum, info : "Mining"});
+                    db.query('INSERT INTO transaction SET ?', { sender: mining_crypto, receiver: mining_crypto, amount: amountMining, date: datum, info: "Mining" });
                 });
-    
-                
-                
-            } 
+
+
+
+            }
         })
 
 
-    }) 
-    
+    })
+
 }
 exports.auszahlen = (req, res) => {
     var amountAuszahlen = req.body.auszahlen;
     var amountMining = parseFloat(amountAuszahlen);
     var email = show.email;
-    
 
-    db.query("SELECT mining FROM user WHERE email = ?" , [email] , function (err,result){
-        if(err){
+
+    db.query("SELECT mining FROM user WHERE email = ?", [email], function (err, result) {
+        if (err) {
             console.log(err);
         }
-        
-        if(amountMining <= result[0].mining){
+
+        if (amountMining <= result[0].mining) {
             var neuesMining = result[0].mining - amountMining;
-        
-            db.query("SELECT kontostand FROM user WHERE email = ?", [email], function (err, result){
-                if(err){
+
+            db.query("SELECT kontostand FROM user WHERE email = ?", [email], function (err, result) {
+                if (err) {
                     console.log(err);
                 }
                 var kontostand = result[0].kontostand;
                 console.log(kontostand);
                 var neuerKontostand = kontostand + amountMining;
                 console.log(neuerKontostand);
-                db.query("UPDATE user SET ? WHERE email = ?", [{mining : neuesMining}, email]);
-                db.query("UPDATE user SET ? WHERE email = ?", [{Kontostand : neuerKontostand}, email]);
+                db.query("UPDATE user SET ? WHERE email = ?", [{ mining: neuesMining }, email]);
+                db.query("UPDATE user SET ? WHERE email = ?", [{ Kontostand: neuerKontostand }, email]);
 
-                res.render("wallet",{
+                res.render("wallet", {
                     message5: "Auszahlung erfolgreich!!"
                 })
 
-                db.query("SELECT crypto FROM user WHERE email = ?", [email] , function (err,result) {
-                    if(err){
+                db.query("SELECT crypto FROM user WHERE email = ?", [email], function (err, result) {
+                    if (err) {
                         console.log(err);
                     }
                     var auszahlen_crypto = result[0].crypto;
                     var datum = new Date();
-                     db.query('INSERT INTO transaction SET ?', { sender: auszahlen_crypto, receiver: auszahlen_crypto, amount: amountMining, date: datum ,info: "Auszahlen"  });
+                    db.query('INSERT INTO transaction SET ?', { sender: auszahlen_crypto, receiver: auszahlen_crypto, amount: amountMining, date: datum, info: "Auszahlen" });
                 });
             })
         }
-        else{
-            res.render("wallet",{
+        else {
+            res.render("wallet", {
                 message6: "Sie haben nicht so viel gemint!!"
             })
             console.log("Sie haben nicht so viel gemint!!");
         }
-      
+
     })
 
 }
