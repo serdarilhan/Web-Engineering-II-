157d4163ed09d9116e51b496d307b918ab27f158
Elian <elian.yildirim@web.de>
2021-04-23 02:31:24 +0200

    Headers Fehler ausgebessert

--- controllers/auth.js
+++ controllers/auth.js
@@ -76,51 +76,6 @@ exports.index = (req, res) => {
     }
     register();
 
-
-
-
-    // db.query('SELECT email FROM user WHERE email = ?', [email], async (error, results) => {
-    //     if (error) {
-    //         console.log(error);
-    //     }
-    //     if (results.length > 0) {
-    //         return res.render("index", {
-    //             message: " Email schon vergeben"
-    //         })
-    //     } else if (passwort !== passwortWiederholen) {
-    //         return res.render("index", {
-    //             message: "Passwort stimmt nicht überein!"
-    //         })
-    //     } else {
-    //         var kontostand = 100;
-    //         var mining = 0;
-    //         let hashedPasswort = await bcrypt.hash(passwort, 8);
-    //         console.log(hashedPasswort);
-    //         const current_date = (new Date()).valueOf().toString();
-    //         const random = Math.random().toString();
-    //         const cryptoHashed = crypto.createHash('sha256').update(current_date + random).digest('hex');
-    //         console.log(cryptoHashed);
-
-    //         db.query("INSERT INTO user SET ? ", { name: name, email: email, passwort: hashedPasswort, crypto: cryptoHashed, kontostand: kontostand, mining : mining }, (error, results) => {
-    //             if (error) {
-    //                 console.log(error);
-    //             }
-    //             else {
-    //                 console.log(results);
-    //                 return res.render("index", {
-    //                     message: "User ist regestriert"
-
-    //                 })
-
-    //             }
-
-    //         })
-
-    //      res.status(200).redirect("../login");
-    //     }
-    // });
-
-
 }
 
 exports.login = async (req, res) => {
@@ -128,11 +83,11 @@ exports.login = async (req, res) => {
 
         var { email, passwort } = req.body;
 
-        if (!email || !passwort) {
-            return res.status(401).render('login', {
-                message: "Schauen Sie noch einmal!"
-            })
-        }
+        // if (!email || !passwort) {
+        //     return res.status(401).render('login', {
+        //         message: "Schauen Sie noch einmal!"
+        //     })
+        // }
 
         async function login() {
             const emailvorhanden = await prisma.$queryRaw('SELECT email FROM user WHERE email = ?', email);
@@ -149,16 +104,16 @@ exports.login = async (req, res) => {
                     const token = jwt.sign({ id: id }, process.env.JWT_SECRET, {
                         expiresIn: process.env.JWT_EXPIRES_IN
                     });
-                    console.log("Token ist:" + token);
+                    console.log("Token ist: " + token);
                     const cookieoptions = {
                         expires: new Date(
                             Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
                         ),
                         httpOnly: true
                     }
-                    res.status(200).redirect("../home");
                     res.cookie("jwt", token, cookieoptions);
-                    res.send("jwt: " + token);
+                    res.status(200).redirect("../home");
+                    //res.send("jwt: " + token);
                 }
                 return email;
             } else {
